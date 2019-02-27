31f3fa3ac3c3074b207ffe3800c351ad8900ac3c
reneeb <info@perl-services.de>
2019-02-27 07:45:42 +0100

    next step for reminder mail

--- reminder_mails.pl
+++ reminder_mails.pl
@@ -3,44 +3,134 @@
 use strict;
 use warnings;
 
+use utf8;
+
 use Email::Sender::Transport::SMTP;
 use Email::Stuffer;
 use File::Spec;
 use File::Basename;
 use Mojo::Template;
 use Net::Netrc;
+use Text::CSV_XS;
 
-use feature 'say';
+use constant LEVEL_1      => 5;
+use constant INPUT_FILE   => File::Spec->catfile( dirname(__FILE__), 'Dienstplan.csv');
+use constant ADDRESS_FILE => File::Spec->catfile( dirname(__FILE__), 'addresses.txt');
+use constant TEMPLATE     => File::Spec->catfile( dirname(__FILE__), 'drill_mail.ep' );
 
-my $machine = Net::Netrc->lookup( 'ffw.mail' );
-my $login   = $machine->login;
-my $passwd  = $machine->password;
+my %groups         = _get_groups();
+my ($group, $text) = _get_group_and_mail_text();
 
-my %vars;
-my $template = File::Spec->catfile( dirname(__FILE__), 'mongers.ep' );
-my $text     = Mojo::Template->new( vars => 1 )->render_file( $template, \%vars );
+exit if !$text;
 
-my $file = '.txt';
-if ( open my $fh, '<', $file ) {
+my $to = join ', ', @{ $groups{$group} || [] };
+exit if !$to;
+
+#_send_mail( $to, $text );
+
+exit;
+
+# -----
+
+
+
+sub _send_mail {
+    my ($to, $text) = @_;
+
+    my ($subject, $mailtext) = split /\n\n/, $text, 2;
+
+    my $machine = Net::Netrc->lookup( 'ffw.mail' );
+    my $login   = $machine->login;
+    my $passwd  = $machine->password;
+    my $host    = $machine->account;
+
+    Email::Stuffer->from     ( $login )
+                  ->to       ( $to )
+                  ->subject  ( $subject )
+                  ->text_body( $mailtext )
+                  ->transport( Email::Sender::Transport::SMTP->new( {
+                      host          => $host,
+                      sasl_username => $login,
+                      sasl_password => $password,
+                  }) )
+                  ->send;
+}
+
+
+sub _get_drill {
+    open my $fh, '<:encoding(utf-8)', INPUT_FILE;
+    return if !$fh;
+
+    my $csv = Text::CSV_XS->new ({ binary => 1, auto_diag => 1 });
+
+    my $is_list;
+    my $drill;
+    my $today = int( time / 86_400 );
+
+    while ( my $row = $csv->getline( $fh ) ) {
+        $is_list++   if $row->[1] eq 'Datum';
+        $is_list = 0 if !$row->[1];
+
+        next if !$is_list;
+        next if $row->[1] eq 'Datum';
+
+        my $date   = Time::Piece->strptime( $row->[1], '%d.%m.%y' );
+        my $epoche = int ( $date->epoch / 86_400 );
+
+        if ( $epoche - $today == 5 ) {
+            $drill = {
+                group => $row->[4],
+                type  => $row->[5],
+                time  => $row->[2],
+                date  => $row->[1],
+                resp  => $row->[6],
+                topic => $row->[3],
+            };
+
+            last;
+        }
+    }
+    
+    return $drill;
+}
+
+sub _get_group_and_mail_text {
+    my $drill = _get_drill();
+
+    return if !$drill;
+
+    my %vars = (
+        type  => 'die nächste Übung',
+        date  => $drill->{date},
+        resp  => $drill->{responsible},
+        topic => $drill->{topic},
+    );
+
+    if ( $drill->{type} eq 'Theorie' ) {
+        $vars{type} = 'der nächste Schulungsabend';
+    }
+
+    my $text = Mojo::Template->new( vars => 1 )->render_file( TEMPLATE, \%vars );
+
+    return $drill->{group}, $text;
+}
+
+sub _get_groups {
+    open my $fh, '<', ADDRESS_FILE;
+    return if !$fh;
+
+    my %groups;
     while ( my $line = <$fh> ) {
         chomp $line;
         next if !$line;
-        next if grep{ $line eq $_ }@not;
-        next if @only and !grep{ $line eq $_ }@only;
 
-        say "Send mail to $line...";
-        Email::Stuffer->from     ( 'pm.list@perl-services.de' )
-                      ->to       ( $line )
-                      ->subject  ( $subject . "" )
-                      ->text_body( $text )
-                      ->transport( Email::Sender::Transport::SMTP->new( {
-                          host          => 'mail.perl-services.de',
-                          sasl_username => 'reb@perl-services.de',
-                          sasl_password => $password . '',
-                      }) )
-                      ->send;
+        my ($mail, $membership) = split /\s/, $line, 2;
 
-        sleep 2;
+        $membership //= 'EA';
+
+        push $groups{$_}, $mail for split /\s*,\s*/, $membership;
     }
+
+    return %groups;
 }
 
